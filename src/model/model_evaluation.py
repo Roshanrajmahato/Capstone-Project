542e32efbd9e2b84d1e83d9c70d53ccc4d7f26ea
Roshanrajmahato <roshanrajmahato9204@gmail.com>
2025-06-21 22:38:44 +0530

    rerun

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -132,7 +132,7 @@ def main():
                     mlflow.log_param(param_name, param_value)
             
             # Log model to MLflow
-            mlflow.sklearn.log_model(clf, "model")
+            mlflow.sklearn.log_model(clf, name="model")
             
             # Save model info
             save_model_info(run.info.run_id, "model", 'reports/experiment_info.json')
